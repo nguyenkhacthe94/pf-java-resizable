0b5c7811f063539c03906413cb5eab7c1833f6c1
khacthe <nguyenkhacthe94@gmail.com>
2018-04-20 09:15:01 +0700

    resize shape

---
+++ ShapeTest.java
@@ -0,0 +1,12 @@
+public class ShapeTest {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+        shape = new Shape("red", false);
+        System.out.println(shape);
+
+        shape.resize(Math.random()*100);
+        System.out.println(shape);
+    }
+}
